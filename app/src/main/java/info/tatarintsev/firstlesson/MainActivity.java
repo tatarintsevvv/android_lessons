5927b7984360a5559586000b4c55f647f56ceb6b
tatarintsevvv <tatarintsev_vv@mail.ru>
2021-03-19 23:18:10 +0300

    Заполнил массив заметок данными из ресурсного файла

--- app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
+++ app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
@@ -4,7 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.text.ParseException;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static NoticeData[] m_notices;
+
+    {
+        // немного хардкода
+        m_notices = new NoticeData[3];
+        String[] titles = getResources().getStringArray(R.array.title);
+        String[] descriptions = getResources().getStringArray(R.array.description);
+        String[] dates = getResources().getStringArray(R.array.where);
+        for (int i = 0; i < 3; i++) {
+            try {
+                m_notices[i] = new NoticeData(titles[i], descriptions[i], dates[i]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
