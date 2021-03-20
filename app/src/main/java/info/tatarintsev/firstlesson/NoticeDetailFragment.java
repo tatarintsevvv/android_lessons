30b72439bd805d4be768d9aaa2dab6057dadd58c
tatarintsevvv <tatarintsev_vv@mail.ru>
2021-03-20 11:02:30 +0300

    DatePicker устанавливается из обьекта типа Date посредством использования типа Calendar

--- app/src/main/java/info/tatarintsev/firstlesson/NoticeDetailFragment.java
+++ app/src/main/java/info/tatarintsev/firstlesson/NoticeDetailFragment.java
@@ -66,10 +66,18 @@ public class NoticeDetailFragment extends Fragment {
         textView.setTextSize(24);
 
         Date dateOfNotice = notice.getDateCreate();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateOfNotice);
         DatePicker datePicker = view.findViewById(R.id.date_detail);
-        datePicker.init(2020, 10, 10, () ->{
+        datePicker.init(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-        });
+                    }
+                });
         return view;
     }
 }
