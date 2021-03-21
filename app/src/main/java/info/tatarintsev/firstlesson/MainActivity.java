e3f81dee7c4551ab76c6a0f8745e48885cec58b1
tatarintsevvv <tatarintsev_vv@mail.ru>
2021-03-21 20:08:34 +0300

    Добавлен тулбар и соотвенно изменены макеты чтобы былои меню

--- app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
+++ app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
         // немного хардкода
         m_notices = new NoticeData[3];
         String[] titles = getResources().getStringArray(R.array.title);
