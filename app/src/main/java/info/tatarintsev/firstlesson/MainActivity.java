7027e206c5822fd18912ce277c347a10504ed0f5
tatarintsevvv <tatarintsev_vv@mail.ru>
2021-03-23 12:37:57 +0300

    NoticeSourceImpl, класс поставщик данных

--- app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
+++ app/src/main/java/info/tatarintsev/firstlesson/MainActivity.java
@@ -33,18 +33,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // немного хардкода
-        m_notices = new NoticeData[3];
-        String[] titles = getResources().getStringArray(R.array.title);
-        String[] descriptions = getResources().getStringArray(R.array.description);
-        String[] dates = getResources().getStringArray(R.array.where);
-        for (int i = 0; i < 3; i++) {
-            try {
-                m_notices[i] = new NoticeData(titles[i], descriptions[i], dates[i]);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
 
         initView();
 
