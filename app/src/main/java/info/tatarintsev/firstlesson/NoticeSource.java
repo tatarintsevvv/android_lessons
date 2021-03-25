f06678462068853fe0775a7b56b773445ebf0788
tatarintsevvv <tatarintsev_vv@mail.ru>
2021-03-25 23:51:46 +0300

    добавлены мтоды к интерфейсу DataSource и сделана рализация

--- app/src/main/java/info/tatarintsev/firstlesson/NoticeSource.java
+++ app/src/main/java/info/tatarintsev/firstlesson/NoticeSource.java
@@ -3,4 +3,8 @@ package info.tatarintsev.firstlesson;
 public interface NoticeSource {
     NoticeData getNoticeData(int position);
     int size();
+    void deleteNoticeData(int position);
+    void updateNoticeData(int position, NoticeData noticeData);
+    void addNoticeData(NoticeData noticeData);
+    void clearNoticeData();
 }
